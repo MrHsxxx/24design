2840e82cd07046dcbae93b8671d114dc347475e2
sheep <285845975@qq.com>
2019-10-23 19:47:52 +0800

    冲突4

--- etc/src/factories/abstracts/CarFactory.java
+++ etc/src/factories/abstracts/CarFactory.java
@@ -2,13 +2,10 @@ package factories.abstracts;
 
 public interface CarFactory {
 
-<<<<<<< HEAD
     int i = 0;
     int b = 1;
-=======
     int f = 2;
     int c = 3;
     String ste = "asdasdads";
     int k = 5;
->>>>>>> hsx
 }
